30f3a6570e6456b4ce870ed2e166730963715e10
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-02-20 11:25:19 +0700

    [update] index.html, main.js, {delete: baseline_favorite_black_18dp.png}

--- 62130500039_groupwork_4/main.js
+++ 62130500039_groupwork_4/main.js
@@ -1,77 +1,112 @@
-    class Member{
-        constructor(text){
-            this.text = text;
-        }
+class Member {
+    constructor(text) {
+        this.text = text;
     }
-    
-    const app = {
-        data() {
-            return {
-                memberList: [{
-                        image: './images/miyeon.jpg',
-                        text: 'MIYEON',
-                        like: false,
-                        unlike: true
-                    },
-                    {
-                        image: './images/minnie.jpg',
-                        text: 'MINNIE',
-                        like: false,
-                        unlike: true
-                    },
-                    {
-                        image: './images/soojin.jpg',
-                        text: 'SOOJIN',
-                        like: false,
-                        unlike: true
-                    },
-                    {
-                        image: './images/soyeon.jpg',
-                        text: 'SOYEON',
-                        like: false,
-                        unlike: true
-                    },
-                    {
-                        image: './images/yuqi.jpg',
-                        text: 'YUQI',
-                        like: false,
-                        unlike: true
-                    },
-                    {
-                        image: './images/shuhua.jpg',
-                        text: 'SHUHUA',
-                        like: false,
-                        unlike: true
+}
+
+const app = {
+    data() {
+        return {
+            memberList: [{
+                image: './images/miyeon.jpg',
+                text: 'MIYEON',
+                like: false,
+                unlike: true,
+                show: true
+
+            },
+            {
+                image: './images/minnie.jpg',
+                text: 'MINNIE',
+                like: false,
+                unlike: true,
+                show: true
+            },
+            {
+                image: './images/soojin.jpg',
+                text: 'SOOJIN',
+                like: false,
+                unlike: true,
+                show: true
+            },
+            {
+                image: './images/soyeon.jpg',
+                text: 'SOYEON',
+                like: false,
+                unlike: true,
+                show: true
+            },
+            {
+                image: './images/yuqi.jpg',
+                text: 'YUQI',
+                like: false,
+                unlike: true,
+                show: true
+            },
+            {
+                image: './images/shuhua.jpg',
+                text: 'SHUHUA',
+                like: false,
+                unlike: true,
+                show: true
+            }
+            ],
+            search: { clicked: false, notclick: true },
+            searchText: '',
+            notFound: false
+
+        }
+    },
+    methods: {
+        toggleLike(index) {
+            this.memberList[index].like = !this.memberList[index].like
+            this.memberList[index].unlike = !this.memberList[index].unlike
+
+        },
+        showSearch() {
+            this.search.clicked = !this.search.clicked
+            this.search.notclick = !this.search.notclick
+            this.searchText = ''
+            this.showMember();
+        },
+        showImg(index) {
+            
+        },
+        searchMember(){
+            if(this.searchText){
+                for (let index = 0; index < this.memberList.length; index++) {
+                    const element = this.memberList[index];
+                    if(element.text !== this.searchText.toUpperCase()){
+                        element.show = false
+                        this.notFound = false
                     }
-                ],
-                search: {clicked: false, notclick: true},
-                searchText: ''
+                    if(element.text.includes(this.searchText.toUpperCase())){
+                        element.show = true
+                        this.notFound = false
+                    }
+                    if(this.memberList.every(element => !element.show)){
+                        this.notFound = true
+                    }
+                }
+            }else{
+                this.showMember();
+            }
+        },
+        showMember(){
+            for (let index = 0; index < this.memberList.length; index++) {
+                this.memberList[index].show = true
+                this.notFound = false
                 
             }
-        },
-        methods: {
-            toggleLike(index) {
-                    this.memberList[index].like = !this.memberList[index].like
-                    this.memberList[index].unlike = !this.memberList[index].unlike
-            
-            },
-            showSearch(){
-                this.search.clicked = !this.search.clicked
-                this.search.notclick = !this.search.notclick
-                this.searchText=''
-            }
-            
-        },
-        computed: {
-            countLiked() {
-                return this.memberList.filter( t => t.like ).length
-            },
-            searchMember(){
-                return this.memberList.filter(member =>{
-                    return member.text.toUpperCase().includes(this.searchText.toUpperCase())
-                })
-            }
+        }
+
+    },
+    computed: {
+        countLiked() {
+            return this.memberList.filter(t => t.like).length
         }
     }
+}
 
-    Vue.createApp(app).mount('#app')
+
+Vue.createApp(app).mount('#app')
